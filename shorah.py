820db6ce8fa717310d3ac0361e92c487f2196c09
Osvaldo Zagordi <zagordi.osvaldo@virology.uzh.ch>
2012-09-06 16:53:48 +0200

    Version by Kerensa from shoran-0.6

--- shorah.py
+++ shorah.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2007, 2008, 2009
+# Copyright 2007-2012
 # Niko Beerenwinkel,
 # Nicholas Eriksson,
 # Moritz Gerstung,
@@ -26,6 +26,7 @@ import subprocess
 import os
 import os.path
 import sys
+import dec
 
 import logging
 import logging.handlers
@@ -46,14 +47,14 @@ dn = os.path.dirname(__file__)
 
 def run_f2r(filein):
     """
-    2. translate to read format
-    output: filein.read
-    """
-    
-    my_prog = 'perl -I %s %s' % (dn+'/perllib', os.path.join(dn, 'fas2read.pl'))
-    my_arg  = " -fasta %s" % filein
-    assert os.path.isfile(filein), 'File %s not found' % filein
+        2. translate to read format
+        output: filein.read
+        """
 
+    my_prog = 'perl -I %s %s' % (dn+'/perllib', os.path.join(dn, 'fas2read.pl'))
+    my_arg  = " -f %s" % filein
+    assert os.path.isfile(filein), 'File %s not found' % filein
+    
     # runs the fasta to read conversion
     try:
         retcode = subprocess.call(my_prog + my_arg, shell=True)
@@ -70,14 +71,14 @@ def run_f2r(filein):
 
 def run_contain(filein):
     """
-    3. eliminate redundant reads
-    output: filein.rest
-    """
+        3. eliminate redundant reads
+        output: filein.rest
+        """
     
     my_prog = os.path.join(dn, 'contain')
     my_arg  = " -f %s" % filein
     assert os.path.isfile('%s.fas' % filein), 'File %s not found' % filein
-
+    
     #eliminates redundant reads
     try:
         retcode = subprocess.call(my_prog + my_arg, shell=True)
@@ -91,15 +92,33 @@ def run_contain(filein):
     
     return retcode
 
+def run_snv(reference,bam,sigma,incr):
+    """
+        x. Parse SNVs from local support, run strand bias
+        output: raw_snv, snvs in 2+ windows, snvs + strand test
+        """
+    my_prog = os.path.join(dn, 'snv.py')
+    my_arg = " -r %s -b %s -s %f -i %d" % (reference, bam, sigma, incr)
+    try:
+        retcode = subprocess.call(my_prog + my_arg, shell=True)
+        if retcode < 0:
+            sholog.error(my_prog + my_arg)
+            sholog.error("Child %s was terminated by signal" % my_prog, -retcode)
+        else:
+            sholog.debug("Child %s returned %i" % (my_prog, retcode))
+    except OSError, ee:
+        sholog.error("Execution of %s failed:" % my_prog, ee)
+    
+    return retcode
 
 def run_mm(filein, max_hap=200):
     """
-    4. run maximum matching, output up to 200 haplotypes
-    output: sample.$nr.geno
-    """
+        4. run maximum matching, output up to 200 haplotypes
+        output: sample.$nr.geno
+        """
     my_prog = os.path.join(dn, 'mm.py')
     my_arg  = " %s %i" % ('%s.rest' % filein, max_hap)
-
+    
     assert os.path.isfile('%s.rest' % filein), 'File %s not found' % filein
     
     #minimal coverage of the read graph
@@ -112,19 +131,19 @@ def run_mm(filein, max_hap=200):
             sholog.debug("Child %s returned %i" % (my_prog, retcode))
     except OSError, ee:
         sholog.error("Execution of %s failed:" % my_prog, ee)
-
+    
     return retcode
 
 
 def run_freqEst(filein):
     """
-    5. run EM
-    output: sample.$nr.popl
-    """
+        5. run EM
+        output: sample.$nr.popl
+        """
     my_prog = os.path.join(dn, 'freqEst')
     my_arg  = " -f %s" % filein
     assert os.path.isfile('%s.rest' % filein), 'File %s not found' % filein
-
+    
     #estimate frequencies
     try:
         retcode = subprocess.call(my_prog + my_arg, shell=True)
@@ -135,99 +154,99 @@ def run_freqEst(filein):
             sholog.debug("Child %s returned %i" % (my_prog, retcode))
     except OSError, ee:
         sholog.error("Execution of %s failed:" % my_prog, ee)
-
+    
     return retcode
 
 
 def main():
     """ Only called if run not interactively
-    """
+        """
     import optparse
     import shutil
+    import glob
     import dec
-    import s2f
     
     # parse command line
     optparser = optparse.OptionParser()
     
-    optparser.add_option("-f", "--readsfile", help="file with reads <.fas or .far format>",
+    optparser.add_option("-b", "--bam", help="sorted bam format alignment file.",
+                         default="", type="string", dest="b")
+    optparser.add_option("-f", "--fasta", help="reference genome in fasta format.",
                          default="", type="string", dest="f")
-    optparser.add_option("-j", "--iterations", help="iterations in dpm sampling <1000>", default=1000,
-                         type="int", dest="j")
     optparser.add_option("-a", "--alpha", help="alpha in dpm sampling <0.01>", default=0.01,
                          type="float", dest="a")
-    optparser.add_option("-w", "--windowsize", help="window size in <201>", default=201,
+    optparser.add_option("-w", "--windowsize", help="window size <201>", default=201,
                          type="int", dest="w")
-    optparser.add_option("-t","--threshold", help="if similarity is less, throw reads away... <default=0.7>", type="float",
-                         dest="threshold", default=0.7)
-    optparser.add_option("-p","--pad_insert",help="insert padding gaps <default=not insert>", action="store_true", default=False,
-                         dest="pad")
-    optparser.add_option("-r","--ref", type="string", default="",
-                         dest="ref")
     optparser.add_option("-s", "--winshifts", help="number of window shifts <3>", default=3,
                          type="int", dest="s") # window shiftings, such that each base is covered up to win_shifts times
-    optparser.add_option("-k","--keep_files",help="keep intermediate files (Gibbs sampling) <default=False>", default=False,
+    optparser.add_option("-i", "--sigma", help="value of sigma to use when calling SNVs", default = 0.01,
+                         type="float", dest="i")
+    optparser.add_option("-x", "--maxcov", help="approximate maximum coverage allowed", default=10000,
+                         type="int", dest="x")
+    optparser.add_option("-r", "--region", help="region in format 'chr:start-stop', eg 'ch3:1000-3000'", default='',
+                         type="string", dest="r")
+    optparser.add_option("-k","--keep_files",help="keep intermediate files (Gibbs sampling)", default=False,
                          action="store_true", dest="k")
-    
     (options, args) = optparser.parse_args()
     
     sholog.info(' '.join(sys.argv))
-    in_file = options.f
+    in_bam = options.b
+    in_fasta = options.f
     keep_all_files = options.k
     step = options.w
     win_shifts = options.s
-    iters = options.j
+    max_coverage = options.x
+    sigma = options.i
     alpha = options.a
-    
+    region = options.r
+
+    in_stem=os.path.split(in_bam)[1].split('.')[0]
+
+    if not os.path.exists('snv/SNV.txt'):
+        # run dec.py
+        sholog.debug('running dec.py')
+        dec.main(in_bam,in_fasta, step, win_shifts, max_coverage, sigma, region, keep_all_files, alpha)
+
+        # copy file and run fas2reads
+        shutil.move('%s.cor.fas' % in_stem, '%s_cor.fas' % in_stem)
+        sholog.debug('running fas2reads')
+
+        retcode = run_f2r('%s_cor.fas' % in_stem)
+        if retcode is not 0:
+            sholog.error('fas2reads did not return 0')
+            sys.exit()
+
+        # run contain
+        retcode = run_contain('%s_cor' % in_stem)
+        if retcode is not 0:
+            sys.exit()
+
+        # run mm.py (this might become a module)
+        retcode = run_mm('%s_cor' % in_stem)
+        if retcode is not 0:
+            sys.exit()
+        
+        # run EM freqEst
+        retcode = run_freqEst('%s_cor' % in_stem)
+        if retcode is not 0:
+            sys.exit()
+
+
+
+    # run snv.py
+    sholog.debug('running snv.py')
+    retcode = run_snv(in_fasta, in_bam, sigma, step/win_shifts)
+    if retcode is not 0:
+        sholog.error('snv calling failed')
+
+    # tidy snvs
     try:
-        [in_stem, in_format] = [os.path.split(in_file)[1].split('.')[0], os.path.split(in_file)[1].split('.')[1]]
-    except IndexError:
-        print 'The input file must be filestem.format'
-        sholog.error('The input file must be filestem.format')
-        sys.exit()
-        
-    if in_format != 'far':
-        ref_file = options.ref
-        out_file = os.path.join(os.getcwd(), in_stem+'.far')
-        thresh = options.threshold
-        if thresh >= 1.0:
-            sys.exit('invalid threshold')
-        pad_insert = options.pad
-        sholog.debug('running s2f.py')
-        s2f.main(ref_file, in_file, out_file, thresh, pad_insert, keep_all_files)
-    
-        # run dec.py
-        sholog.debug('running dec.py')
-        dec.main(out_file, step, win_shifts, keep_all_files, iters, alpha)
-    else:
-        # run dec.py
-        sholog.debug('running dec.py')
-        dec.main(in_file, step, win_shifts, keep_all_files, iters, alpha)
-        
-    # copy file and run fas2reads
-    shutil.move('%s.cor.fas' % in_stem, '%s_cor.fas' % in_stem)
-    sholog.debug('running fas2reads')
-    
-    retcode = run_f2r('%s_cor.fas' % in_stem)
-    if retcode is not 0:
-        sholog.error('fas2reads did not return 0')
-        sys.exit()
-    
-    # run contain
-    retcode = run_contain('%s_cor' % in_stem)
-    if retcode is not 0:
-        sys.exit()
-    
-    # run mm.py (this might become a module)
-    retcode = run_mm('%s_cor' % in_stem)
-    if retcode is not 0:
-        sys.exit()
-    
-    # run EM freqEst
-    retcode = run_freqEst('%s_cor' % in_stem)
-    if retcode is not 0:
-        sys.exit()
-    # goodbye
+        os.mkdir('snv')
+    except:
+        pass
+    for snv_file in glob.glob('./SNV*'):
+        shutil.move(snv_file, 'snv/')
+# goodbye
 
 if __name__ == "__main__":
     main()
