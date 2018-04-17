68ecb4557acfbce899145de1fdaf237d50f7e5b5
ozagordi <osvaldo.zagordi+git@gmail.com>
2018-04-17 16:57:55 +0200

    Shift the files juggling to setuptools
    meson and ninja are not needed to install
    anything. This is now done by setup.py,
    by subclassing develop and install.

--- setup.py
+++ setup.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python
+import glob
 import sys
+import os
+import shutil
+
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+def move_files():
+    """Identify the build directory and copy python files and executables into src/shorah."""
+    try:
+        main_file = glob.glob('*/__main__.py')[0]
+    except IndexError:
+        sys.exit('No __main__.py found. Build first.')
+    build_dir = os.path.dirname(main_file)
+    for py_file in glob.glob('%s/*py' % build_dir):
+        shutil.copy(py_file, 'src/shorah/')
+    for exe in ['b2w', 'diri_sampler', 'fil']:
+        ppp = shutil.copy('%s/src/cpp/%s' % (build_dir, exe), 'src/shorah/bin')
+        print(ppp)
+
+class CustomDevelop(develop):
+    """Subclassing develop to install files in the correct location."""
+    def run(self):
+        move_files()
+        develop.run(self)
+
+
+class CustomInstall(install):
+    """Subclassing develop to install files in the correct location."""
+    def run(self):
+        move_files()
+        install.run(self)
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
@@ -27,7 +59,11 @@ setup(
     setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     install_requires=['setuptools_scm'],
     tests_require=['pytest', 'flake8', 'pep257'],
-    cmdclass={'test': PyTest},
+    cmdclass={
+        'test': PyTest,
+        'install': CustomInstall,
+        'develop': CustomDevelop
+        },
     name='ShoRAH',
     description='SHOrt Reads Assembly into Haplotypes',
     url='http://github.com/cbg-ethz/shorah',
@@ -35,9 +71,6 @@ setup(
     # author_email='firstname.lastname@gmail.com',
     packages=find_packages('src'),  # include all packages under src
     package_dir={'': 'src'},  # tell setuptools packages are under src
-    # package_data={'virmet': ['db/*', 'db/template.tex']},
-    # scripts=['bin/virmet'],
-    # data_files=[('Rscripts', ['Rscripts/covplot.R'])],
     entry_points={
         'console_scripts': ['shorah = shorah.cli:main']
     },
@@ -47,21 +80,22 @@ setup(
     local haplotype reconstruction, and estimation of the frequency of the different genetic variants
     present in a mixed sample.
     ''',
+    project_urls={
+        'Documentation': 'http://cbg-ethz.github.io/shorah',
+        'Source': 'https://github.com/cbg-ethz/shorah',
+    },
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Medical Science Apps.',
-
         # Pick your license as you wish (should match "license" above)
         'License :: GPL 3.0',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
