2a135a0a6879a66fee7ceb74df899ec866cf5931
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-05-29 09:39:37 +0200

    Make shorah base_dir detection slightly more robust

--- src/shorah/cli.py
+++ src/shorah/cli.py
@@ -44,7 +44,7 @@ import sys
 from pkg_resources import (get_distribution, DistributionNotFound)
 
 all_dirs = os.path.abspath(__file__).split(os.sep)
-base_dir = os.sep.join(all_dirs[:all_dirs.index('shorah') + 1])
+base_dir = os.sep.join(all_dirs[:-all_dirs[::-1].index('shorah')])
 
 try:
     __version__ = get_distribution('shorah').version
