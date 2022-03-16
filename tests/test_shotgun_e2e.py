e31fff0319ad7af4316f0efd3de1d30c49136aca
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2022-03-16 14:18:24 +0100

    [Added] SNV.csv and .vcf are written for flexible number of windows covering SNV

--- tests/test_shotgun_e2e.py
+++ tests/test_shotgun_e2e.py
@@ -9,7 +9,7 @@ def test_e2e_shorah():
     assert original.returncode == 0
 
     assert filecmp.cmp(
-        "./data_1/test.csv", 
-        "./data_1/snv/raw_snv_0.010000_final.csv", 
+        "./data_1/test.csv",
+        "./data_1/snv/SNVs_0.010000_final.csv", 
         shallow=False
     )
