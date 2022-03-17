a12be3006e477c4f7527117058b229028c32aa29
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2022-03-17 12:52:29 +0100

    [Add feature] mean field approximation for local haplotype inference

---
+++ shorah/local_haplotype_inference/mean_field_approximation/cavi.py
@@ -0,0 +1,153 @@
+import numpy as np
+from timeit import default_timer as timer
+import pickle
+import pandas as pd
+import multiprocessing as mp
+from scipy.special import digamma
+from scipy.stats._multivariate import _lnB as lnB
+from scipy.special import betaln
+
+
+# my python scripts
+from . import initialization
+from . import update_eqs
+from . import elbo_eqs
+from . import analyze_results
+
+
+"""
+Parallelizing with Pool following:
+https://www.machinelearningplus.com/python/parallel-processing-python/
+"""
+
+results =[]
+
+def collect_result(result):
+    global results
+    results.append(result)
+
+
+def multistart_cavi(K, alpha0, alphabet, reference_binary, reference_seq, reads_list,reads_seq_binary, reads_weights, n_starts, output_dir):
+
+    pool = mp.Pool(mp.cpu_count())
+    for start in range(n_starts):
+        pool.apply_async(run_cavi, args=(K, alpha0, alphabet, reference_binary, reference_seq, reads_list, reads_seq_binary, reads_weights,start, output_dir), callback=collect_result)
+
+    pool.close()
+    pool.join()
+
+    return results
+
+
+def run_cavi(K, alpha0, alphabet, reference_binary, reference_seq, reads_list, reads_seq_binary, reads_weights,start_id, output_dir):
+    """
+    Runs cavi (coordinate ascent variational inference).
+    """
+    start_time =timer()
+    dict_result={'run_id': start_id,
+                 'N': len(reads_list),
+                 'K': K,
+                 'alpha0': alpha0,
+                 'alphabet': alphabet} # N= #reads, K= #components
+
+
+    state_init_dict = initialization.draw_init_state(K, alpha0, alphabet, reads_list, reference_binary)
+
+    state_init_dict.update({'lnB_alpha0': lnB(state_init_dict['alpha']),
+                            'betaln_a0_b0': betaln(state_init_dict['gamma_a'],state_init_dict['gamma_b']),
+                            'betaln_c0_d0': betaln(state_init_dict['theta_c'],state_init_dict['theta_d'])})
+
+    # write initial values to dict
+    dict_result.update({'theta0': state_init_dict['mean_log_theta'][0],
+                        'gamma0': state_init_dict['mean_log_gamma'][0],
+                        'mean_h0': state_init_dict['mean_haplo'],
+                        'mean_z0': state_init_dict['mean_cluster'],
+                        'mean_log_pi': state_init_dict['mean_log_pi']})
+
+    list_history = [state_init_dict]
+    end_time_intit =timer()
+    dict_result.update({'time_initialization': end_time_intit-start_time})
+
+    # Iteratively update mean values
+    iter=0
+    message='' # those can be deleted afterwardsd
+    exitflag=''# those can be deleted afterwardsd
+    converged=False
+    elbo=0
+    elbo_history=[]
+    state_curr_dict = state_init_dict
+    k=0
+
+    while (converged==False) or (k<15):
+
+        if iter<=1:
+            digamma_alpha_sum=digamma(state_curr_dict['alpha'].sum(axis=0))
+            digamma_a_b_sum=digamma(state_curr_dict['gamma_a']+state_curr_dict['gamma_b'])
+            digamma_c_d_sum=digamma(state_curr_dict['theta_c']+state_curr_dict['theta_d'])
+            state_curr_dict.update({'digamma_alpha_sum': digamma_alpha_sum})
+            state_curr_dict.update({'digamma_a_b_sum': digamma_a_b_sum})
+            state_curr_dict.update({'digamma_c_d_sum': digamma_c_d_sum})
+
+        state_curr_dict = update_eqs.update(reads_seq_binary, reads_weights,reads_list, reference_binary, state_init_dict, state_curr_dict)
+        elbo_old = elbo
+        #print('ELBO ',elbo)
+        elbo = elbo_eqs.compute_elbo(reads_weights,reads_seq_binary,reference_binary, state_init_dict, state_curr_dict)
+
+        elbo_history.append(elbo)
+
+        if iter>1:
+            if (elbo_history[-2]>elbo) and np.abs(elbo-elbo_history[-2])>1e-08 :
+                message='Error: ELBO is decreasing.'
+                exitflag=-1
+                break
+            elif np.abs(elbo-elbo_history[-2]) <1e-08:
+                converged=True
+                k+=1
+                message='ELBO converged.'
+                exitflag=0
+            else:
+                k=0
+
+        #if k%10==0: # every 10th parameter set is saved to history
+        state_curr_dict.update({'elbo':elbo})
+        list_history.append(state_curr_dict)
+
+        iter+=1
+    # End: While-loop
+
+    state_curr_dict.update({'elbo':elbo})
+    # save history
+    list_history.append(state_curr_dict)
+    df_history = pd.DataFrame(list_history)
+    df_history.to_csv(output_dir+'history_run'+str(start_id)+'.csv')
+
+    end_time_optimization = timer()
+
+    dict_result.update({'exit_message': message,
+                        'exitflag': exitflag,
+                        'n_iterations': iter,
+                        'converged': converged,
+                        'elbo': elbo,
+                        'elbo_history': elbo_history,
+                        'time_optimization': end_time_optimization-end_time_intit,
+                        'runtime_total': end_time_optimization-start_time})
+
+    dict_result.update(state_curr_dict)
+    summary = analyze_results.summarize_results(state_curr_dict,
+                                                         alphabet,
+                                                         reads_seq_binary,
+                                                         reads_weights,
+                                                         reads_list,
+                                                         reference_binary,
+                                                         reference_seq)
+    dict_result.update(summary)
+    state_curr_dict.update(summary)
+
+
+    f2 = open(output_dir+'results'+str(start_id)+'.pkl',"wb")
+    pickle.dump(dict_result,f2)
+    f2.close()
+
+    result=(state_init_dict, state_curr_dict)
+
+    return result
---
+++ shorah/local_haplotype_inference/mean_field_approximation/run_dpm_mfa.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import skbio
+import numpy as np
+import sys
+import os
+import pickle
+from timeit import default_timer as timer
+
+# my python-scripts
+from . import preparation
+from . import update_eqs as update_eqs
+from . import initialization
+from . import analyze_results
+from . import elbo_eqs
+from . import cavi
+
+def gzip_file(f_name):
+    """Gzip a file and return the name of the gzipped, removing the original
+    """
+    import gzip
+    f_in = open(f_name, 'rb')
+    f_out = gzip.open(f_name + '.gz', 'wb')
+    f_out.writelines(f_in)
+    f_out.close()
+    f_in.close()
+    os.remove(f_in.name)
+
+    return f_out.name
+
+def main(freads_in, fref_in, output_dir, n_starts, K, alpha0, alphabet = 'ACGT-'):
+    start_time = timer()
+
+    window_id = freads_in.split('/')[-1][:-4] # freads_in is absolute path
+    window=[int(window_id.split('-')[2])-1,int(window_id.split('-')[3].split('.')[0])]
+    dict_runtime={'window_id': window_id}
+
+    output_name = output_dir+window_id+'-'
+
+    if os.path.exists(output_dir)==False: # Check whether the specified path exists or not
+        os.makedirs(output_dir) # Create a new directory because it does not exist
+
+    # Read in reads
+    reference_seq, ref_id = preparation.load_reference_seq(fref_in, window) #TODO: check what happens with insertions
+    reference_binary = preparation.reference2binary(reference_seq, alphabet)
+    if freads_in.endswith('fasta') or freads_in.endswith('fas'):
+        reads_list= preparation.load_fasta2reads_list(freads_in, alphabet)
+    elif freads_in.endswith('bam'):
+        reads_list= preparation.load_bam2reads_list(freads_in, alphabet)
+
+    reads_seq_binary, reads_weights = preparation.reads_list_to_array(reads_list)
+
+    end_time_init = timer()
+    dict_runtime.update({'time_preparation': end_time_init-start_time})
+    dict_runtime.update({'n_starts': n_starts})
+    #print('time_preparation', end_time_init-start_time)
+
+    result_list = cavi.multistart_cavi(K, alpha0, alphabet, reference_binary, reference_seq, reads_list, reads_seq_binary, reads_weights, n_starts, output_name)
+
+    end_time_optimization = timer()
+    dict_runtime.update({'time_optimization': end_time_optimization-end_time_init})
+    #print('time_optimization', end_time_optimization-end_time_init)
+
+    # Write summary output file
+    #outfile = open(output_dir+'debug/output_info.txt','w')
+    outfile = open(output_dir+'output_info.txt','w')
+    outfile.write('reference '+ fref_in + '\n')
+    outfile.write('reads '+ freads_in + '\n')
+    outfile.write('lenght of sequences '+ str(reads_list[0].seq_binary.shape[0]) + '\n')
+    outfile.write('number of reads '+ str(len(reads_list)) + '\n')
+    outfile.write('number of components '+ str(K) + '\n')
+
+
+    # Find best run
+    #if n_starts >1:
+    max_elbo = result_list[0][1]['elbo']
+    max_idx = 0
+    for idx, state_run in enumerate(result_list):
+        state_end_dict = state_run[1]
+        curr_elbo = state_end_dict['elbo']
+        if max_elbo < curr_elbo:
+            max_elbo = curr_elbo
+            max_idx = idx
+    #else:
+    #    max_idx=0
+
+
+    state_curr_dict = result_list[max_idx][1]
+    outfile.write('Maximal ELBO '+str(max_elbo) + 'in run '+ str(max_idx) +'\n')
+    outfile.write('Best run -- clustering results: \n')
+    outfile = analyze_results.write_info2file(state_curr_dict, outfile, alphabet, reads_list, reads_seq_binary, reads_weights,reference_binary, reference_seq)
+    outfile.close()
+
+    # write output like in original shorah
+    analyze_results.haplotypes_to_fasta(state_curr_dict, output_name+'support.fas')
+    analyze_results.correct_reads(state_curr_dict, output_name+'cor.fas')
+
+    #analyze_results.haplotypes_to_fasta(state_curr_dict, output_dir+'support/'+fname_out_base+'.reads-support.fas')
+    #analyze_results.correct_reads(state_curr_dict, output_dir+'corrected/'+fname_out_base+'.reads-cor.fas')
+
+    #f_best_run = open(output_dir+'debug/best_run.txt','w')
+    f_best_run = open(output_name+'best_run.txt','w')
+    f_best_run.write(str(max_idx))
+    f_best_run.close()
+
+
+    end_time_total = timer()
+    dict_runtime.update({'time_total': end_time_total-start_time})
+    #print('time_total', end_time_total-start_time)
+
+    #f = open(output_dir+'debug/runtime.pkl',"wb")
+    f = open(output_name+'runtime.pkl',"wb")
+    pickle.dump(dict_runtime,f)
+    f.close()
+
+    # clean up Files
+    if os.path.exists(output_dir+'inference/')==False:
+        os.makedirs(output_dir+'inference/')
+
+    import glob
+    import shutil
+
+    inference_files = glob.glob('./w*best_run.txt') + glob.glob('./w*history_run*.csv') + glob.glob('./w*runtime.pkl') + glob.glob('./w*results*.pkl')
+
+    print(inference_files)
+    for inf_file in inference_files:
+        if os.stat(inf_file).st_size > 0:
+            gzf = gzip_file(inf_file)
+            try:
+                os.remove('inference/%s' % gzf)
+            except OSError:
+                pass
+            shutil.move(gzf, 'inference/')
+        else:
+            os.remove(inf_file)
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]), sys.argv[7])
+#freads_in, fref_in, output_dir, n_starts, K, alpha0, alphabet = 'ACGT-'
