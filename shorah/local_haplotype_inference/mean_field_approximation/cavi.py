ec5588d1ab0ec5120340b34ab64e893bd7a811bf
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-07-19 13:55:45 +0200

    refactor and delete

--- shorah/local_haplotype_inference/mean_field_approximation/cavi.py
+++ shorah/local_haplotype_inference/mean_field_approximation/cavi.py
@@ -1,5 +1,4 @@
 import numpy as np
-from timeit import default_timer as timer
 import pickle
 import pandas as pd
 import multiprocessing as mp
@@ -20,16 +19,43 @@ https://www.machinelearningplus.com/python/parallel-processing-python/
 
 results = []
 
+
 def collect_result(result):
     global results
     results.append(result)
 
 
-def multistart_cavi(K, alpha0, alphabet, reference_binary, reads_list,reads_seq_binary, reads_weights, reads_log_error_proba, n_starts, output_dir):
+def multistart_cavi(
+    K,
+    alpha0,
+    alphabet,
+    reference_binary,
+    reads_list,
+    reads_seq_binary,
+    reads_weights,
+    reads_log_error_proba,
+    n_starts,
+    output_dir,
+):
 
     pool = mp.Pool(mp.cpu_count())
     for start in range(n_starts):
-        pool.apply_async(run_cavi, args=(K, alpha0, alphabet, reference_binary, reads_list, reads_seq_binary, reads_weights, reads_log_error_proba, start, output_dir), callback=collect_result)
+        pool.apply_async(
+            run_cavi,
+            args=(
+                K,
+                alpha0,
+                alphabet,
+                reference_binary,
+                reads_list,
+                reads_seq_binary,
+                reads_weights,
+                reads_log_error_proba,
+                start,
+                output_dir,
+            ),
+            callback=collect_result,
+        )
 
     pool.close()
     pool.join()
@@ -37,18 +63,30 @@ def multistart_cavi(K, alpha0, alphabet, reference_binary, reads_list,reads_seq_
     return results
 
 
-def run_cavi(K, alpha0, alphabet, reference_binary, reads_list, reads_seq_binary, reads_weights, reads_log_error_proba, start_id, output_dir):
+def run_cavi(
+    K,
+    alpha0,
+    alphabet,
+    reference_binary,
+    reads_list,
+    reads_seq_binary,
+    reads_weights,
+    reads_log_error_proba,
+    start_id,
+    output_dir,
+):
     """
     Runs cavi (coordinate ascent variational inference).
     """
-    start_time =timer()
-    dict_result={'run_id': start_id,
-                 'N': len(reads_list),
-                 'K': K,
-                 'alpha0': alpha0,
-                 'alphabet': alphabet} # N= #reads, K= #components
+    dict_result = {
+        "run_id": start_id,
+        "N": len(reads_list),
+        "K": K,
+        "alpha0": alpha0,
+        "alphabet": alphabet,
+    }  # N= #reads, K= #components
 
-    history_alpha= []
+    history_alpha = []
     history_mean_log_pi = []
     history_gamma_a = []
     history_gamma_b = []
@@ -57,90 +95,115 @@ def run_cavi(K, alpha0, alphabet, reference_binary, reads_list, reads_seq_binary
     history_mean_cluster = []
     history_elbo = []
 
-    state_init_dict = initialization.draw_init_state(K, alpha0, alphabet, reads_list, reference_binary)
-    state_init_dict.update({'lnB_alpha0': lnB(state_init_dict['alpha']),
-                            'betaln_a0_b0': betaln(state_init_dict['gamma_a'],state_init_dict['gamma_b'])})
+    state_init_dict = initialization.draw_init_state(
+        K, alpha0, alphabet, reads_list, reference_binary
+    )
+    state_init_dict.update(
+        {
+            "lnB_alpha0": lnB(state_init_dict["alpha"]),
+            "betaln_a0_b0": betaln(
+                state_init_dict["gamma_a"], state_init_dict["gamma_b"]
+            ),
+        }
+    )
 
     # write initial values to dict
-    dict_result.update({'gamma0': state_init_dict['mean_log_gamma'][0],
-                        'mean_h0': state_init_dict['mean_haplo'],
-                        'mean_z0': state_init_dict['mean_cluster'],
-                        'mean_log_pi': state_init_dict['mean_log_pi']})
-
-    end_time_intit =timer()
-    dict_result.update({'time_initialization': end_time_intit-start_time})
+    dict_result.update(
+        {
+            "gamma0": state_init_dict["mean_log_gamma"][0],
+            "mean_h0": state_init_dict["mean_haplo"],
+            "mean_z0": state_init_dict["mean_cluster"],
+            "mean_log_pi": state_init_dict["mean_log_pi"],
+        }
+    )
 
     # Iteratively update mean values
-    iter=0
-    message='' # those can be deleted afterwardsd
-    exitflag=''# those can be deleted afterwardsd
-    converged=False
-    elbo=0
+    iter = 0
+    message = ""  # those can be deleted afterwardsd
+    exitflag = ""  # those can be deleted afterwardsd
+    converged = False
+    elbo = 0
     state_curr_dict = state_init_dict
-    while (converged==False):
+    while converged == False:
 
-        if iter<=1:
-            digamma_alpha_sum=digamma(state_curr_dict['alpha'].sum(axis=0))
-            digamma_a_b_sum=digamma(state_curr_dict['gamma_a']+state_curr_dict['gamma_b'])
-            state_curr_dict.update({'digamma_alpha_sum': digamma_alpha_sum})
-            state_curr_dict.update({'digamma_a_b_sum': digamma_a_b_sum})
+        if iter <= 1:
+            digamma_alpha_sum = digamma(state_curr_dict["alpha"].sum(axis=0))
+            digamma_a_b_sum = digamma(
+                state_curr_dict["gamma_a"] + state_curr_dict["gamma_b"]
+            )
+            state_curr_dict.update({"digamma_alpha_sum": digamma_alpha_sum})
+            state_curr_dict.update({"digamma_a_b_sum": digamma_a_b_sum})
 
-        state_curr_dict = update_eqs.update(reads_seq_binary, reads_weights, reference_binary, reads_log_error_proba, state_init_dict, state_curr_dict)
-        elbo = elbo_eqs.compute_elbo(reads_weights,reference_binary, reads_log_error_proba,state_init_dict, state_curr_dict)
+        state_curr_dict = update_eqs.update(
+            reads_seq_binary,
+            reads_weights,
+            reference_binary,
+            reads_log_error_proba,
+            state_init_dict,
+            state_curr_dict,
+        )
+        elbo = elbo_eqs.compute_elbo(
+            reads_weights,
+            reference_binary,
+            reads_log_error_proba,
+            state_init_dict,
+            state_curr_dict,
+        )
 
         history_elbo.append(elbo)
-        history_mean_log_pi.append(state_curr_dict['mean_log_pi'])
-        history_mean_log_gamma.append(state_curr_dict['mean_log_gamma'])
-        history_mean_cluster.append(state_curr_dict['mean_cluster'])
+        history_mean_log_pi.append(state_curr_dict["mean_log_pi"])
+        history_mean_log_gamma.append(state_curr_dict["mean_log_gamma"])
+        history_mean_cluster.append(state_curr_dict["mean_cluster"])
 
-        if iter>1:
-            if (history_elbo[-2]>elbo) and np.abs(elbo-history_elbo[-2])>1e-08 :
-                message='Error: ELBO is decreasing.'
-                exitflag=-1
+        if iter > 1:
+            if (history_elbo[-2] > elbo) and np.abs(elbo - history_elbo[-2]) > 1e-08:
+                message = "Error: ELBO is decreasing."
+                exitflag = -1
                 break
-            elif np.abs(elbo-history_elbo[-2]) <1e-03:
-                converged=True
-                message='ELBO converged.'
-                exitflag=0
+            elif np.abs(elbo - history_elbo[-2]) < 1e-03:
+                converged = True
+                message = "ELBO converged."
+                exitflag = 0
 
-        state_curr_dict.update({'elbo':elbo})
+        state_curr_dict.update({"elbo": elbo})
 
-        iter+=1
+        iter += 1
     # End: While-loop
 
-    state_curr_dict.update({'elbo' : elbo})
-    end_time_optimization = timer()
+    state_curr_dict.update({"elbo": elbo})
 
-    dict_result.update({'exit_message': message,
-                        'exitflag': exitflag,
-                        'n_iterations': iter,
-                        'converged': converged,
-                        'elbo': elbo,
-                        'history_elbo': history_elbo,
-                        'history_alpha': history_alpha,
-                        'history_mean_log_pi': history_mean_log_pi,
-                        'history_alpha': history_alpha,
-                        'history_gamma_a': history_gamma_a,
-                        'history_gamma_b': history_gamma_b,
-                        'history_mean_log_gamma': history_mean_log_gamma,
-                        'history_mean_haplo': history_mean_haplo,
-                        'history_mean_cluster': history_mean_cluster,
-                        'time_optimization': end_time_optimization-end_time_intit,
-                        'runtime_total': end_time_optimization-start_time})
+    dict_result.update(
+        {
+            "exit_message": message,
+            "exitflag": exitflag,
+            "n_iterations": iter,
+            "converged": converged,
+            "elbo": elbo,
+            "history_elbo": history_elbo,
+            "history_alpha": history_alpha,
+            "history_mean_log_pi": history_mean_log_pi,
+            "history_alpha": history_alpha,
+            "history_gamma_a": history_gamma_a,
+            "history_gamma_b": history_gamma_b,
+            "history_mean_log_gamma": history_mean_log_gamma,
+            "history_mean_haplo": history_mean_haplo,
+            "history_mean_cluster": history_mean_cluster,
+        }
+    )
 
-
-
-    summary = analyze_results.summarize_results(state_curr_dict,
-                                                alphabet,
-                                                reads_seq_binary,
-                                                reads_weights,
-                                                reads_list,
-                                                reads_log_error_proba,
-                                                reference_binary)
+    summary = analyze_results.summarize_results(
+        state_curr_dict,
+        alphabet,
+        reads_seq_binary,
+        reads_weights,
+        reads_list,
+        reads_log_error_proba,
+        reference_binary,
+    )
 
     dict_result.update(summary)
     state_curr_dict.update(summary)
 
-    result=(state_init_dict, state_curr_dict, dict_result)
+    result = (state_init_dict, state_curr_dict, dict_result)
 
     return result
--- shorah/local_haplotype_inference/mean_field_approximation/initialization.py
+++ shorah/local_haplotype_inference/mean_field_approximation/initialization.py
@@ -3,68 +3,77 @@ from scipy.special import digamma
 
 from . import update_eqs as update_eqs
 
-def draw_init_state(K, alpha0,alphabet,reads_list, reference_binary):
 
-    L= reads_list[0].seq_binary.shape[0] # length of seq
-    N= len(reads_list) # number of reads
-    B= len(alphabet) # number of letters in alphabet
+def draw_init_state(n_clusters, alpha0, alphabet, reads_list, reference_binary):
 
-    # fixed parameters
-    alpha_temp=alpha0*np.ones(K) # concentration parameter for Dirichlet prior of components
+    genome_length = reads_list[0].seq_binary.shape[0]
+    n_reads = len(reads_list)
+    size_alphabet = len(alphabet)
+
+    # concentration parameter for Dirichlet prior of components
+    alpha_temp = alpha0 * np.ones(n_clusters)
 
     # initialization of mean values
-    digamma_alpha_sum=digamma(alpha_temp.sum(axis=0))
-    mean_log_pi = update_eqs.get_mean_log_pi(alpha_temp,digamma_alpha_sum)
+    digamma_alpha_sum = digamma(alpha_temp.sum(axis=0))
+    mean_log_pi = update_eqs.get_mean_log_pi(alpha_temp, digamma_alpha_sum)
 
-    matches, mismatch = count_mis_and_matches_wrt_ref(reads_list, reference_binary)
-    matches =10
+    matches = 10
     mismatch = 2
 
-    k = np.random.uniform(low=0.5, high=1.0,size=4)
-    a,b = matches*k[0], mismatch*k[1]
-    c,d = matches*k[2], mismatch*k[3]
+    k = np.random.uniform(low=0.5, high=1.0, size=4)
+    a, b = matches * k[0], mismatch * k[1]
+    c, d = matches * k[2], mismatch * k[3]
 
-    gamma0 = np.random.beta(a,b)
+    gamma0 = np.random.beta(a, b)
+    mean_log_gamma = np.log(gamma0), np.log(1 - gamma0)
+    mean_h = init_mean_haplo(
+        n_clusters, genome_length, size_alphabet, mean_log_gamma, reference_binary
+    )
+    mean_z = np.random.dirichlet(alpha_temp, size=n_reads)
 
-    mean_log_gamma = np.log(gamma0), np.log(1-gamma0)
-
-    mean_h = init_mean_haplo(K,L,alphabet,mean_log_gamma, reference_binary)
-
-    mean_z = np.ones((N,K))/K
-    for n in range(N):
-        mean_z[n]=np.random.dirichlet((alpha_temp)*100)
-
-    state_init_dict = dict({'alpha': alpha_temp,
-                            'mean_log_pi': mean_log_pi,
-                            'gamma_a': a,
-                            'gamma_b': b,
-                            'mean_log_gamma': mean_log_gamma,
-                            'mean_haplo': mean_h,
-                            'mean_cluster': mean_z })
+    state_init_dict = dict(
+        {
+            "alpha": alpha_temp,
+            "mean_log_pi": mean_log_pi,
+            "gamma_a": a,
+            "gamma_b": b,
+            "mean_log_gamma": mean_log_gamma,
+            "mean_haplo": mean_h,
+            "mean_cluster": mean_z,
+        }
+    )
 
     return state_init_dict
 
-def count_mis_and_matches_wrt_ref(reads_list, reference_table):
-    matches=0
-    mismatch=0
-    totbase=0
-    for n in range(len(reads_list)): # iterate over reads
-        matches+=reads_list[n].weight*(np.multiply(reference_table,reads_list[n].seq_binary).sum(axis=0).sum(axis=0))
-        #for i in range(reference_table.shape[1]): # iterate over base
-        #    for l in range(reference_table.shape[0]): # iterate over positions
-        #        matches0+=reference_table[l][i]*reads_list[n].seq_binary[l][i]*reads_list[n].weight
-        totbase+=reads_list[n].weight*reads_list[n].n_non_N
 
-    mismatch=totbase-matches
+def count_mis_and_matches_wrt_ref(reads_list, reference_table):
+    matches = 0
+    mismatch = 0
+    totbase = 0
+    for n in range(len(reads_list)):  # iterate over reads
+        matches += reads_list[n].weight * (
+            np.multiply(reference_table, reads_list[n].seq_binary)
+            .sum(axis=0)
+            .sum(axis=0)
+        )
+        totbase += reads_list[n].weight * reads_list[n].n_non_N
+
+    mismatch = totbase - matches
 
     return matches, mismatch
 
-def init_mean_haplo(K,L,alphabet,mean_log_gamma, reference_table):
-    base_true = np.exp(mean_log_gamma[0])
-    base_false = (1.-np.exp(mean_log_gamma[1]))/(len(alphabet)-1)
-    mean_haplo = np.ones((K,L,len(alphabet)))
-    for k in range(K):
-        for position in range(L):
-            for base in range(len(alphabet)):
-                mean_haplo[k][position][base]=(base_true**reference_table[position][base])*(base_false**(1-reference_table[position][base]))
-    return mean_haplo
+
+def init_mean_haplo(
+    n_clusters, genome_length, size_alphabet, mean_log_gamma, reference_table
+):
+    base_true = np.exp(mean_log_gamma[0]) * np.ones(
+        (n_clusters, genome_length, size_alphabet)
+    )
+    base_false = (
+        (1.0 - np.exp(mean_log_gamma[1]))
+        / (size_alphabet - 1)
+        * np.ones((n_clusters, genome_length, size_alphabet))
+    )
+    return np.power(base_true, reference_table) * np.power(
+        base_false, 1 - reference_table
+    )
