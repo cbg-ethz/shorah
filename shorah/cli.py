336df2d75b09b49b9cdaa4b74f104a4b64e36166
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-08-19 14:33:19 +0200

    argparse-command-line-flags-without-arguments

--- shorah/cli.py
+++ shorah/cli.py
@@ -184,8 +184,7 @@ def main():
                                 required=False, default=1, dest="n_mfa_starts",
                                 help="Number of starts for inference type mean_field_approximation.")
 
-    parser_shotgun.add_argument("--unique_modus", metavar='Bool', type=bool,
-                                required=False, default=True, dest="unique_modus",
+    parser_shotgun.add_argument('--non-unique_modus', action='store_false', dest="unique_modus",
                                 help="For inference: Make read set unique with read weights.")
 
 
